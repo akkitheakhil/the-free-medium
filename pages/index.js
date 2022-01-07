b212e71a976a49f9afc3331520b2c18a204a34fd
akkitheakhil <akhilpad007@gmail.com>
2022-01-07 22:36:56 +0530

    fix: trying fix for firebase nextjs issue

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import Head from 'next/head'
 import FeedPage from '@screens/Feed';
-import { db, postToJSON } from '@lib/firebase-helper';
-import { collectionGroup, limit, orderBy, getDocs, query } from 'firebase/firestore';
+import { db, postToJSON, } from '@lib/firebase-helper';
+import { getFirestore, collectionGroup, limit, orderBy, getDocs, query } from 'firebase/firestore';
 import Metatags from '@components/Metatags';
+import app from '@lib/firebase';
 
 export async function getServerSideProps(context) {
-
-  const collectionRef = collectionGroup(db, 'posts');
-  const queryColl = query(collectionRef, orderBy('createdAt', 'desc'), limit(10));
+  const queryColl = query(collectionGroup(getFirestore(app), 'posts'), orderBy('createdAt', 'desc'), limit(10));
   const snapshot = await getDocs(queryColl);
   const posts = snapshot.docs.map(postToJSON);
   return {
